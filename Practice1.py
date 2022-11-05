ccbe06c98afc1b4fc129bb56efc780837ddaa56f
ajay-ar30 <ajayaroraN1@gmail.com>
2022-11-05 11:16:05 -0500

    working with variables in python

---
+++ Practice1.py
@@ -0,0 +1,22 @@
+#Variable assignments#
+
+#can reassign variables to different values
+var1 = 1
+
+var1 = ["abc", "def"]
+
+#can check variable type using type() method
+
+var1 = var1 + var1
+
+print("New var1 value is ",var1)
+
+#New var1 value is  ['abc', 'def', 'abc', 'def']
+
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income * tax_rate
+
+print("Applicable taxes are", my_taxes)
