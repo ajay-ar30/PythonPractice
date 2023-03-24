5b3cedeccd062f54181d4443e4fd5993e4cd7a26
ajay-ar30 <ajayaroraN1@gmail.com>
2023-03-24 12:22:56 -0500

    Modified OOP_homework.py

--- OOP_homework.py
+++ OOP_homework.py
@@ -1,7 +1,7 @@
 '''Problem 1
 Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.'''
 
-class Line:
+class Line():
     
     def __init__(self,coor1,coor2):
         self.coor1 = coor1
@@ -29,7 +29,7 @@ Problem 2
 Fill in the class
 
 '''
-class Cylinder:
+class Cylinder():
     pi = 3.14
     def __init__(self,height=1,radius=1):
         self.height = height
@@ -44,3 +44,38 @@ class Cylinder:
 c = Cylinder(2,3)
 print(c.volume())
 print(c.surface_area())
+
+
+'''Object Oriented Programming Challenge
+For this challenge, create a bank account class that has two attributes:
+
+owner
+balance
+and two methods:
+
+deposit
+withdraw
+As an added requirement, withdrawals may not exceed the available balance.
+
+Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.'''
+
+class BankAccount():
+    def __init__(self, owner,balance=0):
+        self.owner = owner
+        self.balance = balance
+        print(f"Account owner: {self.owner}\nAccount balance:${self.balance}")
+
+    def deposit(self,dep_amt):
+        self.balance += dep_amt
+        return "Deposit accepted"
+    
+    def withdraw(self, wd_amt):
+        if self.balance >= wd_amt:
+            self.balance -= wd_amt
+            print('Withdrawal Accepted')
+        else:
+            print('Funds Unavailable!')
+acct1 = BankAccount("Jose",100)
+acct1.deposit(5000)
+print(acct1.balance)
+    
