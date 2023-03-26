15fece0fb52d9cc4d14f0d3a68cbd60681b9ff5e
ajay-ar30 <ajayaroraN1@gmail.com>
2023-03-26 04:52:45 -0500

    Explanation added for __name__ == '__main__'

--- one.py
+++ one.py
@@ -7,3 +7,12 @@ if __name__ == '__main__':
     print("ONE.PY is being run directly!")
 else:
     print("ONE.PY is imported")
+
+'''
+Purpose of __name__ == __main__ technique:
+
+Basically you are splitting your .py scripts into two sections: i) function, class and object definitions, ii) running a series of functions and methods.
+Your anticipation is that if you are running the script x directly, you want to execute the functions but if you are just importing the script x, 
+you are only interested in importing the definitions segment from script x to use with function execution in script y.
+
+'''
